ce8ef0ef908f5d04c53aed07dcfbef69e9927804
박상호 <81404342+whos-bax@users.noreply.github.com>
2024-04-30 13:26:43 +0900

    parallel routes -> default.tsx

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -13,3 +13,6 @@ export default function BeforeLoginLayout({ children, modal }: Props) {
         </div>
     )
 }
+
+// 주소가 localhost:3000 일 때는 children->page.tsx, modal->@modal/default.tsx
+// 주소가 localhost:3000/i/flow/login 때는 children->i/flow/login/page.tsx, modal->@modal/i/flow/login/page.tsx
