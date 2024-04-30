87251235793ea57d6c0fc94e3770480809afc696
박상호 <81404342+whos-bax@users.noreply.github.com>
2024-04-30 12:52:43 +0900

    paraller Route

---
+++ src/app/(beforeLogin)/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+import styles from "@/app/page.module.css";
+
+type Props = {
+    children: ReactNode;
+    modal: ReactNode;
+}
+export default function BeforeLoginLayout({ children, modal }: Props) {
+    return (
+        <div className={styles.container}>
+            {children}
+            {modal}
+        </div>
+    )
+}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,4 +12,4 @@ const NotFound: NextPage = () => {
     )
 }
 
-export default NotFound;
+export default NotFound
