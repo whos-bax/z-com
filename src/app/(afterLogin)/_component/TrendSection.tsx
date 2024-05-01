eca38e5b759b3028b7af9bb2c924757d27e47bd3
박상호 <81404342+whos-bax@users.noreply.github.com>
2024-05-01 11:00:35 +0900

    explore page

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import styles from './trendSection.module.css';
 import Trend from "@/app/(afterLogin)/_component/Trend";
+import {usePathname} from "next/navigation";
 
 export default function TrendSection() {
+    const pathname = usePathname();
+    if (pathname === "/explore") return null;
+
     return (
         <div className={styles.trendBg}>
             <div className={styles.trend}>
--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,26 @@
+import styles from "./explore.module.css";
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Trend from "@/app/(afterLogin)/_component/Trend";
+
 export default function Home() {
     return (
-        <main>
-            검색페이지
+        <main className={styles.main}>
+            <div className={styles.formZone}>
+                <SearchForm />
+            </div>
+            <div className={styles.trend}>
+                <h3>나를 위한 트렌드</h3>
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+            </div>
         </main>
     )
 }
