a552d9823ac1109da2e77ce225d75270ebdf3e25
박상호 <81404342+whos-bax@users.noreply.github.com>
2024-05-02 20:23:23 +0900

    react-query

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -3,19 +3,43 @@ import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
 import Post from "@/app/(afterLogin)/_component/Post";
+import {dehydrate, HydrationBoundary, QueryClient} from "@tanstack/react-query";
+// import {revalidatePath, revalidateTag} from "next/cache";
 
-export default function Home() {
+async function getPostRecommends() {
+    const res = await fetch(`http://localhost:9090/api/postRecommends`, {
+        next: {
+            tags: ['posts', 'recommends'],
+        },
+        cache: 'no-store',
+    })
+
+    if (!res.ok) {
+        throw new Error('Failed to fet ch data')
+    }
+    // revalidateTag('posts')
+    // revalidatePath('/home')
+    return res.json();
+}
+
+export default async function Home() {
+    const queryClient = new QueryClient();
+    await queryClient.prefetchQuery({queryKey: ['posts', 'recommends'], queryFn: getPostRecommends})
+    const dehydratedState = dehydrate(queryClient);
+
     return (
         <main className={styles.main}>
-            <TabProvider>
-                <Tab/>
-                <PostForm/>
-                {Array.from({length: 10}).map((_, index) => (
-                    <div key={`post_${index}`}>
-                        <Post/>
-                    </div>
-                ))}
-            </TabProvider>
+            <HydrationBoundary state={dehydratedState}>
+                <TabProvider>
+                    <Tab/>
+                    <PostForm/>
+                    {Array.from({length: 10}).map((_, index) => (
+                        <div key={`post_${index}`}>
+                            <Post/>
+                        </div>
+                    ))}
+                </TabProvider>
+            </HydrationBoundary>
         </main>
     )
 }
--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -9,6 +9,7 @@ import TrendSection from "@/app/(afterLogin)/_component/TrendSection";
 import FollowRecommend from "@/app/(afterLogin)/_component/FollowRecommend";
 import RightSearchZone from "@/app/(afterLogin)/_component/RightSearchZone";
 import {auth} from "@/auth";
+import RQProvider from "@/app/(afterLogin)/_component/RQProvider";
 
 type Props = { children: ReactNode, modal: ReactNode }
 export default async function AfterLoginLayout({children, modal}: Props) {
@@ -45,22 +46,24 @@ export default async function AfterLoginLayout({children, modal}: Props) {
                     </div>
                 </section>
             </header>
-            <div className={style.rightSectionWrapper}>
-                <div className={style.rightSectionInner}>
-                    <main className={style.main}>{children}</main>
-                    <section className={style.rightSection}>
-                        <RightSearchZone/>
-                        <TrendSection/>
-                        <div className={style.followRecommend}>
-                            <h3>팔로우 추천</h3>
-                            <FollowRecommend/>
-                            <FollowRecommend/>
-                            <FollowRecommend/>
-                        </div>
-                    </section>
+            <RQProvider>
+                <div className={style.rightSectionWrapper}>
+                    <div className={style.rightSectionInner}>
+                        <main className={style.main}>{children}</main>
+                        <section className={style.rightSection}>
+                            <RightSearchZone/>
+                            <TrendSection/>
+                            <div className={style.followRecommend}>
+                                <h3>팔로우 추천</h3>
+                                <FollowRecommend/>
+                                <FollowRecommend/>
+                                <FollowRecommend/>
+                            </div>
+                        </section>
+                    </div>
                 </div>
-            </div>
-            {modal}
+                {modal}
+            </RQProvider>
         </div>
     )
 }
