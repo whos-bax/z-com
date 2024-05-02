d0a0d8d26f0c03524aa99ab92ab3bfb57c83ac7d
박상호 <81404342+whos-bax@users.noreply.github.com>
2024-05-02 09:32:18 +0900

    msw setup

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,26 @@
+import {http, HttpResponse} from 'msw';
+import {faker} from "@faker-js/faker";
+
+const User = [
+    {id: 'elonmusk', nickname: 'Elon Musk', image: '/yRsRRjGO.jpg'},
+    {id: 'whosbax', nickname: '호상박', image: '/5Udwvqim.jpg'},
+    {id: 'leoturtle', nickname: '레오', image: faker.image.avatar()},
+]
+export const handlers = [
+    http.post('/api/login', () => {
+        console.log('로그인');
+        return HttpResponse.json(User[1], {
+            headers: {
+                'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
+            }
+        })
+    }),
+    http.post('/api/logout', () => {
+        console.log('로그아웃');
+        return new HttpResponse(null, {
+            headers: {
+                'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0'
+            }
+        })
+    }),
+]
