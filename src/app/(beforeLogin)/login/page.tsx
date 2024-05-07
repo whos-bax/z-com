3419694688a8d9a80f69f180b1d24fcd0efb5753
박상호 <81404342+whos-bax@users.noreply.github.com>
2024-05-07 23:33:15 +0900

    login client -> server

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,20 +1,40 @@
-'use client';
+// 'use client';
+//
+// import { useRouter } from "next/navigation";
+// import Main from "../_component/Main";
+// import {useSession} from "next-auth/react";
+//
+// export default function Login() {
+//   const router = useRouter();
+//   const { data: session } = useSession();
+//
+//   if (session?.user) {
+//     router.replace('/home');
+//     return null;
+//   }
+//
+//   router.replace('/i/flow/login');
+//   return (
+//     <Main />
+//   );
+// }
+import Main from "@/app/(beforeLogin)/_component/Main";
+import {auth} from "@/auth";
+import {redirect} from "next/navigation";
+import RedirectToLogin from "@/app/(beforeLogin)/login/_component/RedirectToLogin";
 
-import { useRouter } from "next/navigation";
-import Main from "../_component/Main";
-import {useSession} from "next-auth/react";
-
-export default function Login() {
-  const router = useRouter();
-  const { data: session } = useSession();
+export default async function Login() {
+  const session = await auth();
 
   if (session?.user) {
-    router.replace('/home');
+    redirect('/home');
     return null;
   }
 
-  router.replace('/i/flow/login');
   return (
-    <Main />
+      <>
+        <RedirectToLogin />
+        <Main/>
+      </>
   );
 }
