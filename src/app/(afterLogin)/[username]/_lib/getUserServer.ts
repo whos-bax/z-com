bdf81f27d364c8642ecebdf5c4ec3c3445c6666d
박상호 <81404342+whos-bax@users.noreply.github.com>
2024-05-07 11:33:44 +0900

    env 처리

--- src/app/(afterLogin)/[username]/_lib/getUserServer.ts
+++ src/app/(afterLogin)/[username]/_lib/getUserServer.ts
@@ -6,7 +6,7 @@ import {cookies} from "next/headers";
 export const getUserServer
     = async ({queryKey}: { queryKey: [string, string] }) => {
     const [_1, username] = queryKey;
-    const res = await fetch(`http://localhost:9090/api/users/${username}`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${username}`, {
         next: {
             tags: ['users', username],
         },
--- src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePostServer.ts
+++ src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePostServer.ts
@@ -6,7 +6,7 @@ import {cookies} from "next/headers";
 export const getSinglePostServer
     = async ({queryKey}: {queryKey: [string, string]}) => {
     const [_1, id] = queryKey;
-    const res = await fetch(`http://localhost:9090/api/posts/${id}`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${id}`, {
         next: {
             tags: ['posts', id],
         },
