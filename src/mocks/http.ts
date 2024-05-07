8f487dd5c1b168b2b1bae9c5a10923de9d0cd322
박상호 <81404342+whos-bax@users.noreply.github.com>
2024-05-08 00:36:42 +0900

    port 변경

--- src/mocks/http.ts
+++ src/mocks/http.ts
@@ -6,7 +6,8 @@ import {handlers} from './handlers';
 const app = express();
 const port = 9090; // server port
 
-app.use(cors({origin: 'http://localhost:3000', optionsSuccessStatus: 200, credentials: true}));
+// app.use(cors({origin: 'http://localhost:3000', optionsSuccessStatus: 200, credentials: true}));
+app.use(cors({origin: 'http://localhost:80', optionsSuccessStatus: 200, credentials: true})); // aws 적용
 app.use(express.json());
 app.use(createMiddleware(...handlers));
 app.listen(port, () => console.log(`Mock server is running on port: ${port}`));
