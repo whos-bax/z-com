1e2bedc6807abc0cc5ff75fe84f1b3b2d658bbf2
박상호 <81404342+whos-bax@users.noreply.github.com>
2024-05-07 23:24:10 +0900

    isr 모드 옵션화

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // output: "export", // static 모드
     async rewrites() {
         return [
             {
                 source: '/upload/:slug',
-                destination: `http://localhost:9090/upload/:slug`
+                destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug`
             }
         ]
     }
